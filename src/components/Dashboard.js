5035f1f9078a3fd23d7a6c19ab4d1f324d90bf73
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-18 16:45:21 -0600

    Import the Loading component and show it when the state is loading

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./Loading";
 
 
 class Dashboard extends Component {
   state = { 
     loading: true 
   };
-  
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+    return this.state.loading ? <Loading /> : <main className={dashboardClasses} />;
   }
 }
 
