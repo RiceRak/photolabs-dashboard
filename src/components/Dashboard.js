890f6c0dccb2c48a964f03bfb4b5e5d65321d5f6
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-20 09:40:21 -0600

    Map over the data array and create a new Panel for each of the four data objects. Render the panels array as children of the main element

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
 
 const data = [
   {
@@ -28,14 +29,27 @@ const data = [
 
 
 class Dashboard extends Component {
-  state = { 
-    loading: true 
+  state = {
+    loading: false
   };
 
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return this.state.loading ? <Loading /> : <main className={dashboardClasses} />;
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = data.map(panel => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
